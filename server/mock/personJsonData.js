b2938d1b0c6479a2be3f86ea3a0b838b411d98e4
gkmallareddy <99544288+gkmallareddy@users.noreply.github.com>
2022-02-12 19:55:26 +0530

    Add files via upload

---
+++ server/mock/personJsonData.js
@@ -0,0 +1,21 @@
+const peronJsonData = {
+  person: [
+    {
+      id: 10,
+      firstName: "John",
+      lastName: "Doe",
+    },
+    {
+      id: 5,
+      firstName: "Jack",
+      lastName: "Doe",
+    },
+    {
+      id: 7,
+      firstName: "James",
+      lastName: "Doe",
+    },
+  ],
+};
+
+module.exports = peronJsonData;
